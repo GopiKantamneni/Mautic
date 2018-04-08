9b4c6321354dc48a316343faf65d44932d0c83cc
Kantamneni <gopi@ibhubs.co>
2018-04-08 10:46:35 +0530

    Added Mautic

---
+++ Desktop/Practice/GIT_MAUTIC/Mautic/app/bundles/LeadBundle/Model/DeviceModel.php
@@ -0,0 +1,178 @@
+<?php
+
+/*
+ * @copyright   2014 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\LeadBundle\Model;
+
+use Mautic\CoreBundle\Helper\CookieHelper;
+use Mautic\CoreBundle\Model\FormModel;
+use Mautic\LeadBundle\Entity\Lead;
+use Mautic\LeadBundle\Entity\LeadDevice;
+use Mautic\LeadBundle\Entity\LeadDeviceRepository;
+use Mautic\LeadBundle\Event\LeadDeviceEvent;
+use Mautic\LeadBundle\LeadEvents;
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+
+/**
+ * Class DeviceModel
+ * {@inheritdoc}
+ */
+class DeviceModel extends FormModel
+{
+    /**
+     * @var CookieHelper
+     */
+    private $cookieHelper;
+
+    /**
+     * @var LeadDeviceRepository
+     */
+    private $leadDeviceRepository;
+
+    /**
+     * @var LeadModel
+     */
+    private $leadModel;
+
+    /**
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * DeviceModel constructor.
+     *
+     * @param CookieHelper         $cookieHelper
+     * @param LeadDeviceRepository $leadDeviceRepository
+     * @param LeadModel            $leadModel
+     * @param Request              $request
+     */
+    public function __construct(
+        CookieHelper $cookieHelper,
+        LeadDeviceRepository $leadDeviceRepository,
+        LeadModel $leadModel,
+        Request $request
+    ) {
+        $this->cookieHelper         = $cookieHelper;
+        $this->leadDeviceRepository = $leadDeviceRepository;
+        $this->leadModel            = $leadModel;
+        $this->request              = $request;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return LeadDeviceRepository
+     */
+    public function getRepository()
+    {
+        return $this->leadDeviceRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getPermissionBase()
+    {
+        return 'lead:leads';
+    }
+
+    /**
+     * Get a specific entity or generate a new one if id is empty.
+     *
+     * @param $id
+     *
+     * @return null|object
+     */
+    public function getEntity($id = null)
+    {
+        if ($id === null) {
+            return new LeadDevice();
+        }
+
+        return parent::getEntity($id);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param       $entity
+     * @param       $formFactory
+     * @param null  $action
+     * @param array $options
+     *
+     * @return mixed
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
+    public function createForm($entity, $formFactory, $action = null, $options = [])
+    {
+        if (!$entity instanceof LeadDevice) {
+            throw new MethodNotAllowedHttpException(['LeadDevice']);
+        }
+
+        if (!empty($action)) {
+            $options['action'] = $action;
+        }
+
+        return $formFactory->create('leaddevice', $entity, $options);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param $action
+     * @param $event
+     * @param $entity
+     * @param $isNew
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException
+     */
+    protected function dispatchEvent($action, &$entity, $isNew = false, Event $event = null)
+    {
+        if (!$entity instanceof LeadDevice) {
+            throw new MethodNotAllowedHttpException(['LeadDevice']);
+        }
+
+        switch ($action) {
+            case 'pre_save':
+                $name = LeadEvents::DEVICE_PRE_SAVE;
+                break;
+            case 'post_save':
+                $name = LeadEvents::DEVICE_POST_SAVE;
+                break;
+            case 'pre_delete':
+                $name = LeadEvents::DEVICE_PRE_DELETE;
+                break;
+            case 'post_delete':
+                $name = LeadEvents::DEVICE_POST_DELETE;
+                break;
+            default:
+                return null;
+        }
+
+        if ($this->dispatcher->hasListeners($name)) {
+            if (empty($event)) {
+                $event = new LeadDeviceEvent($entity, $isNew);
+                $event->setEntityManager($this->em);
+            }
+
+            $this->dispatcher->dispatch($name, $event);
+
+            return $event;
+        } else {
+            return null;
+        }
+    }
+}
